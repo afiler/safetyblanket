0596ed05df538e8cfe5bb927de7f9cbf5dc3efcd
Andrew Filer <afiler@gmail.com>
2013-09-23 20:02:35 -0700

    Added support for checking retval against a block

--- example.rb
+++ example.rb
@@ -6,6 +6,18 @@ module SafetyBlanket
   def set(symbol)
     safetyblanketize symbol, Set
   end
+  
+  def PosInt(symbol)
+    safetyblanketize symbol, Fixnum do |retval|
+      retval > 0
+    end
+  end
+  
+  def Palindrome(symbol)
+    safetyblanketize symbol, String do |str|
+      true
+    end
+  end
 end
 
 class Cruft
@@ -15,9 +27,21 @@ class Cruft
     puts arg
     arg
   end
+  
+  char def first_letter(arg)
+    puts arg[0]
+    arg[0]
+  end
+  
+  PosInt def bar(arg)
+    puts arg
+    arg
+  end
 end
 
 cruft = Cruft.new
 cruft.foo 37
 cruft.foo 23
+cruft.first_letter "pie"
+cruft.bar -20
 cruft.foo "monkey"
--- safetyblanket.rb
+++ safetyblanket.rb
@@ -1,15 +1,35 @@
 module SafetyBlanket
-  def safetyblanketize(symbol, type)
+  def safetyblanketize(symbol, type=nil, &block)
     original_method = "_unsafe_#{symbol}"
+    
+    type_check = %Q{
+      if not retval.is_a? #{type.name}
+        raise RuntimeError, "#{symbol} returned #{'#{retval.class}'} instead of #{type}"
+      end
+    }
+    
+    block_check = if block_given?
+      class_variable_set '@@_safety_checks', {} if not class_variable_defined? '@@_safety_checks' 
+      class_variable_get('@@_safety_checks')[symbol] = block
+      %Q{
+        safety_check = @@_safety_checks[:#{symbol}]
+        if not safety_check.call(retval)
+          raise RuntimeError, "#{symbol} fails return value test at #{'#{safety_check.source_location.first}'}:#{'#{safety_check.source_location.last}'}"
+        end
+      }
+    end   
 
     class_eval %Q{
       raise "Already safetyblanketed #{symbol}" if method_defined?(:#{original_method})
       alias #{original_method} #{symbol}
-
+      
       def #{symbol}(*args)
         retval = #{original_method}(*args)
-        return retval unless !retval.is_a? #{type.name}
-        raise RuntimeError, "#{symbol} returned #{'#{retval.class}'} instead of #{type}"
+        
+        #{type_check}
+        #{block_check}
+        
+        retval
       end
     }
   end
@@ -24,6 +44,12 @@ module SafetyBlanket
   
   def float(symbol)
     safetyblanketize symbol, Float
+  end
+  
+  def char(symbol)
+    safetyblanketize symbol, String do |str|
+      str.length == 1
+    end
   end
 end
 
